694eb5ce0e65f3e25b4d2142983c60ea485c0669
nhi <nguyenthinhi0602@gmail.com>
2019-11-18 19:20:37 +0700

    auth4

--- src/main/java/com/backend/helpdesk/controller/AuthenticationController.java
+++ src/main/java/com/backend/helpdesk/controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.backend.helpdesk.controller;
 import com.backend.helpdesk.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationService authenticationService;
 
+    @Secured("ROLE_EMPLOYEES")
     @GetMapping
     public ResponseEntity<?> login(@RequestHeader("token-google") String tokenGoogle) throws IOException, GeneralSecurityException {
         String email = authenticationService.getEmailFromTokenUser(tokenGoogle);
