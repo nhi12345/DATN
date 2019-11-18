1c80c84eafc046fc953c3915c44e6b55baebb8e1
nhi <nguyenthinhi0602@gmail.com>
2019-11-18 19:05:44 +0700

    auth2

--- src/main/java/com/backend/helpdesk/DTO/Token.java
+++ src/main/java/com/backend/helpdesk/DTO/Token.java
@@ -2,8 +2,11 @@ package com.backend.helpdesk.DTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Token {
     String accessToken;
 }
